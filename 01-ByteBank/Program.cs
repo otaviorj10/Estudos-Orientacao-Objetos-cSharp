16da4e980ee7e836fa2ff603cc9b986960253f86
Luiz Otavio <luiz_otavio71@hotmail.com>
2021-10-10 17:40:09 -0300

    dando inicio

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -6,7 +6,15 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string titular = "gabriela";
+            int numeroAgencia = 863;
+            int numero = 863146;
+            double saldo = 100;
+
+            string titular2 = "bruno";
+            int numeroagencia2 = 863;
+            int numero2 = 864;
+            double saldo2 = 50;
         }
     }
 }
